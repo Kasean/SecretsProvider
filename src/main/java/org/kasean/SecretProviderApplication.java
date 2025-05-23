c505502708221d4e2f84557bf0d7fb5833a79864
Siarhei_Kakichau <siarhei_kakichau@epam.com>
2025-05-23 16:22:49 +0300

    init main class #1

---
+++ src/main/java/org/kasean/SecretProviderApplication.java
@@ -0,0 +1,12 @@
+package org.kasean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretProviderApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SecretProviderApplication.class, args);
+    }
+}
